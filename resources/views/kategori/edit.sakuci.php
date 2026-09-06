bda763f30531ff753ee5a2b35862b0f8ac51bc84
Rizalweh <rijalaudi2605@gmail.com>
2026-09-06 23:55:34 +0700

    latihan nambah form edit

---
+++ resources/views/kategori/edit.sakuci.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title', config('app.name') . ' -- Kerangka PHP Ringan')
+
+@section('content')
+<form action="{{ route('admin.kategori.update', ['id' => $data->id_kategori]) }}" method="post" class="d-flex flex-column form-horizontal">
+    @csrf
+    @method('PUT')
+
+    <label>Nama Kategori</label>
+    <input type="text" name="nama_kategori" value="{{ $data->nama_kategori }}" class="form-control" required>
+
+    <label>Kode Kategori</label>    
+    <input type="text" name="kode_kategori" value="{{ $data->kode_kategori }}" class="form-control" required>
+
+    <label>Keterangan</label>    
+    <input type="text" name="keterangan" value="{{ $data->keterangan }}" class="form-control mb-3" required>
+
+    <button type="submit" class="btn btn-primary">Simpan</button>
+</form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,8 @@ Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
 
     Route::get('/database/export', [DatabaseController::class, 'export'])->name('admin.database.export');
     Route::get('/kategori', [KategoriController::class, 'index'])->name('admin.kategori.index');
+    Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('admin.kategori.edit');
+    Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('admin.kategori.update');
 });
 
 /*

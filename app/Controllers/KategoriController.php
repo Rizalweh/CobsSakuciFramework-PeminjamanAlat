c66ba337b2fbb0810db033a492b8d460ef887116
Rizalweh <rijalaudi2605@gmail.com>
2026-09-06 11:35:30 +0700

    gass latihann mau nyoba nambah edit

---
+++ app/Controllers/KategoriController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use Sakuci\Controller;
+use Sakuci\Http\Request;
+use App\Models\Kategori;
+
+class KategoriController extends Controller
+{
+    public function index(Request $request)
+    {
+        $data = Kategori::paginate(2);
+       return view('kategori.index', compact('data'));
+    }
+}
---
+++ resources/views/kategori/index.sakuci.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title', config('app.name') . ' -- Kerangka PHP Ringan')
+
+@section('content')
+<table class = "table table-sm align-middle"> 
+<tr>
+    <th>No</th>
+    <th>nama kategori</th>
+    <th>kode kategori</th>
+    <th>Keterangan</th>
+    <th>Aksi</th>
+</tr>
+@php $no = 1; @endphp
+@foreach ($data as $kategoris)
+<tr>
+    <td> {{ $no++ }} </td>
+    <td> {{ $kategoris->nama_kategori }} </td>
+    <td> {{ $kategoris->kode_kategori }} </td>
+    <td> {{ $kategoris->keterangan }} </td>
+    <td>
+         <a href="" class="btn btn-primary btn-sm">Edit</a>
+         <a href="" class="btn btn-danger btn-sm">Hapus</a>
+    </td>
+</tr>
+@endforeach
+</table>
+{!! $data->links() !!}
+@endsection
